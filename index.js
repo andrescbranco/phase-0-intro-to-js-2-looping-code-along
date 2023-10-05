b1daa117b66b1ae5eddefbe9e4c78da5430298d3
Andre Schnyder Castello Branco <andrecb18@yahoo.com.br>
2023-10-05 05:11:19 -0700

    Creates looping functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let thankYouMessage = [];
+
+function writeCards (strings, event) {    
+
+    for (let i = 0; i < strings.length; i++) {
+        let messages = `Thank you, ${strings[i]}, for the wonderful ${event} gift!`;
+        thankYouMessage.push(messages);
+    }
+
+    console.log(thankYouMessage);
+    return thankYouMessage;
+    
+}
+
+function countDown(number) {
+    let countDown = number
+    while (countDown >= 0 ) {
+        console.log(countDown--)
+    }
+}
+
